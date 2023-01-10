43be101efa338298c2b51fbe8b7d06b385cde425
iamhassan <hassan.elouardy06@gmail.com>
2023-01-10 13:45:20 +0100

    Add calculator script

---
+++ src/components/calculator.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+export class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calc">
+        <div className="screen">
+          {' '}
+          <p>0</p>
+        </div>
+        <button type="button" className="btn num">
+          AC
+        </button>
+        <button type="button" className="btn num">
+          +/-
+        </button>
+        <button type="button" className="btn num">
+          %
+        </button>
+        <button type="button" className="btn oper">
+          /
+        </button>
+
+        <button type="button" className="btn num">
+          7
+        </button>
+        <button type="button" className="btn num">
+          8
+        </button>
+        <button type="button" className="btn num">
+          9
+        </button>
+        <button type="button" className="btn oper">
+          x
+        </button>
+
+        <button type="button" className="btn num">
+          4
+        </button>
+        <button type="button" className="btn num">
+          5
+        </button>
+        <button type="button" className="btn num">
+          6
+        </button>
+        <button type="button" className="btn oper">
+          -
+        </button>
+
+        <button type="button" className="btn num">
+          1
+        </button>
+        <button type="button" className="btn num">
+          2
+        </button>
+        <button type="button" className="btn num">
+          3
+        </button>
+        <button type="button" className="btn oper">
+          +
+        </button>
+
+        <button type="button" className="btn num zero">
+          0
+        </button>
+        <button type="button" className="btn num">
+          .
+        </button>
+        <button type="button" className="btn oper ">
+          =
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
