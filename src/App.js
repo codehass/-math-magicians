33bf686f010f8e7e2a052489ddd8403e488a37b7
codehass <hassan.elouardy06@gmail.com>
2023-01-12 17:14:53 +0100

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="app">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
