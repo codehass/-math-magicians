a3768b3d62a2416467eab32d9334c04d8bc13faa
codehass <hassan.elouardy06@gmail.com>
2023-01-12 17:31:55 +0100

    Fixe errors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Calculator from './components/Calculator';
+// eslint-disable-next-line import/extensions
+import Calculator from './components/Calculator.js';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
